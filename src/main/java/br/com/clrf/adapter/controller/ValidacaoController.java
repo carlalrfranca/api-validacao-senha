c5b0d2723ba480cc15d46766e8b5b7657b5f5387
Carla Leticia Ribeiro Franca <carlainlondon@protonmail.com>
2026-03-24 22:52:48 -0300

    refactor: centraliza validacoes e excecoes com tratamento global

--- src/main/java/br/com/clrf/adapter/controller/ValidacaoController.java
+++ src/main/java/br/com/clrf/adapter/controller/ValidacaoController.java
@@ -2,37 +2,72 @@ package br.com.clrf.adapter.controller;
 
 import br.com.clrf.adapter.dto.CredenciaisEntrada;
 import br.com.clrf.adapter.dto.ValidacaoResultado;
-import br.com.clrf.adapter.response.MensagemEmail;
-import br.com.clrf.adapter.response.MensagemSenha;
 import br.com.clrf.usecase.ValidadorCredenciaisService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/credenciais")//@RequestMapping("/senhas")
+@RequestMapping("/credenciais")
 @RequiredArgsConstructor
 public class ValidacaoController {
 
     private final ValidadorCredenciaisService validadorCredenciaisService;
 
     @PostMapping("/validacoes")
-    public ResponseEntity<ValidacaoResultado> validar (
-            @Valid @RequestBody CredenciaisEntrada credenciais) {
-
-        Optional<String> erroSenha = validadorCredenciaisService.executaRegrasSenha(credenciais.senha());
-        Optional<String> erroEmail = validadorCredenciaisService.executaRegrasEmail(credenciais.email());
-        boolean valido = erroSenha.isEmpty() && erroEmail.isEmpty();
-
-        String mensagem;
-        if (valido) mensagem = "Credenciais validadas com sucesso";
-        else if (erroSenha.isPresent()) mensagem = MensagemSenha.extraiRegra(erroSenha.get());
-        else mensagem = MensagemEmail.extraiRegra(erroEmail.get());
-        return ResponseEntity.ok(new ValidacaoResultado(valido, mensagem));
+    public ResponseEntity<ValidacaoResultado> validar(@Valid @RequestBody CredenciaisEntrada credenciais) {
+        validadorCredenciaisService.validarCredenciais(credenciais);
+        return ResponseEntity.ok(new ValidacaoResultado(true, "Credenciais validadas com sucesso"));
     }
 }
+
+/*
+package br.com.clrf.adapter.controller;
+
+import br.com.clrf.adapter.dto.CredenciaisEntrada;
+import br.com.clrf.adapter.dto.ValidacaoResultado;
+import br.com.clrf.adapter.response.MensagemEmail;
+import br.com.clrf.adapter.response.MensagemSenha;
+import br.com.clrf.usecase.ValidadorCredenciaisService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
+
+@SuppressWarnings("checkstyle:Indentation")
+@RestController
+@RequestMapping("/credenciais")
+@RequiredArgsConstructor
+public class ValidacaoController {
+
+    private final ValidadorCredenciaisService validadorCredenciaisService;
+
+    @PostMapping("/validacoes")
+    public ResponseEntity<ValidacaoResultado> validar(
+            @Valid @RequestBody CredenciaisEntrada credenciais) {
+
+        Optional<String> erroSenha =
+                validadorCredenciaisService.executaRegrasSenha(credenciais.senha());
+        Optional<String> erroEmail =
+                validadorCredenciaisService.executaRegrasEmail(credenciais.email());
+        boolean valido = erroSenha.isEmpty() && erroEmail.isEmpty();
+        if (valido) {
+            return ResponseEntity.ok(
+                    new ValidacaoResultado(true, "Credenciais validadas com sucesso")
+            );
+        }
+        String mensagem;
+
+        if (erroSenha.isPresent())
+            mensagem = MensagemSenha.extraiRegra(erroSenha.get());
+        else
+            mensagem = MensagemEmail.extraiRegra(erroEmail.get());
+        return ResponseEntity.unprocessableEntity()
+                .body(new ValidacaoResultado(false, mensagem));
+    }
+}
+*/
--- src/main/java/br/com/clrf/usecase/ValidadorCredenciaisService.java
+++ src/main/java/br/com/clrf/usecase/ValidadorCredenciaisService.java
@@ -1,5 +1,54 @@
 package br.com.clrf.usecase;
 
+import br.com.clrf.adapter.dto.CredenciaisEntrada;
+import br.com.clrf.adapter.exception.ExcecoesGlobais.RegraNegocioException;
+import br.com.clrf.adapter.response.MensagemEmail;
+import br.com.clrf.adapter.response.MensagemSenha;
+import br.com.clrf.domain.comuns.policy.PoliticaRegra;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ValidadorCredenciaisService {
+
+    private final PoliticaRegra politicaSenha;
+    private final PoliticaRegra politicaEmail;
+
+    public void validarCredenciais(CredenciaisEntrada credenciais) {
+        List<String> erros = new ArrayList<>();
+
+        if (credenciais.senha() == null) {
+            erros.add("Senha não pode ser nula");
+        } else {
+            politicaSenha.satisfazRegra(credenciais.senha())
+                    .ifPresent(e -> erros.add(MensagemSenha.extraiRegra(e)));
+        }
+
+        if (credenciais.email() == null) {
+            erros.add("Email não pode ser nulo");
+        } else {
+            politicaEmail.satisfazRegra(credenciais.email())
+                    .ifPresent(e -> erros.add(MensagemEmail.extraiRegra(e)));
+        }
+
+        if (!erros.isEmpty()) {
+            log.warn("Erros de validação: {}", erros);
+            throw new RegraNegocioException(String.join("; ", erros));
+        }
+
+        log.info("Credenciais atendem todas as regras de negócio");
+    }
+}
+
+
+/*
+package br.com.clrf.usecase;
+
 import br.com.clrf.domain.comuns.policy.PoliticaRegra;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -32,3 +81,4 @@ public class ValidadorCredenciaisService {
         return regraNaoSatisfeita;
     }
 }
+*/
