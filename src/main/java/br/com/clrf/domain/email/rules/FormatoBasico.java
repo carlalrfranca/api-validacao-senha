1d03c2de80370734784ba12dc2efc5e2c6ad55ab
Carla Leticia Ribeiro Franca <carlainlondon@protonmail.com>
2026-03-26 09:23:16 -0300

    feat: melhora validacao de email com regras mais restritivas e tratamento de casos invalidos

--- src/main/java/br/com/clrf/domain/email/rules/FormatoBasico.java
+++ src/main/java/br/com/clrf/domain/email/rules/FormatoBasico.java
@@ -3,17 +3,20 @@ package br.com.clrf.domain.email.rules;
 import br.com.clrf.domain.comuns.rules.RegraValidacao;
 
 public class FormatoBasico implements RegraValidacao {
-
     @Override
     public boolean valida(String valor) {
+        if (valor == null || valor.isBlank()) return false;
 
-        int indiceArroba = valor.indexOf('@');
-        if(indiceArroba < 0) return false;
-        if (indiceArroba == 0) return false;
-        if (indiceArroba == valor.length() - 1)
-            return false;
-        return true;
+        int arroba = valor.indexOf('@');
+        if (arroba <= 0 || arroba == valor.length() - 1) return false;
+
+        String dominio = valor.substring(arroba + 1);
+        int ponto = dominio.lastIndexOf('.');
+        if (ponto <= 0 || ponto == dominio.length() - 1) return false;
+
+        String local = valor.substring(0, arroba);
+        String tld = dominio.substring(ponto + 1);
+
+        return !local.isBlank() && !tld.isBlank() && tld.length() >= 2;
     }
 }
-
-
